49ccbe84672b82abab9bee789c28a957b3803d11
htb <htb@gmail.com>
2022-07-13 15:43:27 +0700

    BMI for human

--- BMI.cpp
+++ BMI.cpp
@@ -16,11 +16,12 @@ void BMI(ConNguoi nguoi){
     nguoi.W = nguoi.can_nang / (nguoi.chieu_cao*nguoi.chieu_cao);
     cout << nguoi.W << endl;
     if(nguoi.W <= 15) cout << "Thieu can rat nang";
-    else if (15 < nguoi.W <= 16) cout << "thieu can nang";
-    else if (16 < nguoi.W <= 18.5) cout << "thieu can";
-    else if (18.5 < nguoi.W < 25) cout << "binh thuong";
+    else if (15 < nguoi.W && nguoi.W < 16) cout << "thieu can nang";
+    else if (16 < nguoi.W && nguoi.W < 18.5) cout << "thieu can";
+    else if (18.5 < nguoi.W && nguoi.W < 25) cout << "binh thuong";
     else if (nguoi.W == 25) cout <<  "thua can";
-    else if (25 < nguoi.W <= 30) cout << "Tien beo phi";
+    else if (25 < nguoi.W && nguoi.W < 30) cout << "Tien beo phi";
+    else cout << "you good";
     // else if ();
 }
 int main(){
